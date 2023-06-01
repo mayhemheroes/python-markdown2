f1d3828f25f32350d1dfac26a0c7e514779944bf
Bailey Capuano <bcapuano@asu.edu>
2023-05-31 21:44:11 -0400

    Speed up

--- mayhem/fuzz_md.py
+++ mayhem/fuzz_md.py
@@ -8,12 +8,12 @@ with atheris.instrument_imports(include=['markdown2']):
     from markdown2 import Markdown
     import markdown2
 
+markdowner = Markdown()
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     if fdp.ConsumeBool():
         markdown2.markdown(fdp.ConsumeRemainingString())
     else:
-        markdowner = Markdown()
         markdowner.convert(fdp.ConsumeRemainingString())
 def main():
     atheris.Setup(sys.argv, TestOneInput)
